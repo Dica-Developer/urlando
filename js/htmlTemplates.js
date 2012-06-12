6d00d4b3ae2c8afea5694a6d03a008c70ccb4588
Jörg Weber <weber_joerg.h@web.de>
2012-06-12 22:18:55 +0200

    *style options page

--- js/htmlTemplates.js
+++ js/htmlTemplates.js
@@ -1,8 +1,6 @@
 (function($){
-  var optionUrlMarkup = '<input type="text" size="50" value="${url}"><a href="#" onclick="addUrl()">Plus</a><br>';
-  var optionUrlEmptyMarkup = '<input type="text" size="50""><a href="#" onclick="addUrl()">Plus</a><br>';
+  var optionUrlMarkup = '<div class="urlInputDiv"><input type="text" size="50" class="urlInput"><div class="plus addRemoveIcons"></div><div class="minus addRemoveIcons"></div></div>';
   var iFrameMarkup = '<div id="step_${idx}" class="iFrames" data-x="${x}" data-y="${y}"><iframe id="iFrame_${idx}" src="${url}" width="${width}" height="${height}"></iframe></div>';
   $.template('optionUrls', optionUrlMarkup);
-  $.template('optionUrlsEmpty', optionUrlEmptyMarkup);
   $.template('iFrame', iFrameMarkup);
 }(jQuery));
