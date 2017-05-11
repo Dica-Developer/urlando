87395e9865a1c9f43341f31815c303a46709cddf
Jörg Weber <weber.joerg.h@gmail.com>
2017-05-11 23:16:58 +0200

    * rewrite to es6 * remove vendor prefix from css * implement #9 * use class to avoid global variables and it's flaws

--- js/main.js
+++ js/main.js
@@ -1,112 +1,92 @@
-/*global window, chrome, Mousetrap*/
-var intervalTimer = null;
-var nextIframe = 0;
-var options = {
-  reload: false,
-  nrOfiFrames: 0,
-  duration: 10000,
-  resolution: 'full',
-  ratio: 0,
-  random: false,
-  animation: true,
-  chromeScalingFix: false
-};
-var overviewMode = false;
-var iFrameMarkupTemplate = '<div id="step_${idx}" class="iFrames" data-x="${x}" data-y="${y}"><webview id="iFrame_${idx}" data-url="${url}" src="${url}" style="width:${width}px; height:${height}px;"></webview></div>';
+const bindKey = Mousetrap.bind;
 
-function displayStatus(status) {
-  $('<div id="statusMessage" class="statusMessage">' + status + '</div>').appendTo('body');
-  $('#statusMessage').animate({
-    opacity: 1
-  }, 1250, 'swing', function () {
-    $('#statusMessage').animate({
-      opacity: 0
-    }, 1250, 'swing', function () {
-      $('#statusMessage').remove();
-    });
-  });
+function getIframeMarkup(options) {
+    const {
+        idx,
+        x,
+        y,
+        url,
+        width,
+        height
+    } = options;
+
+    return `
+        <div id="step_${idx}" class="iFrames" data-x="${x}" data-y="${y}">
+            <webview id="iFrame_${idx}" data-url="${url}" src="${url}" style="width:${width}px; height:${height}px;"></webview>
+        </div>
+    `;
 }
 
 function addIFrames(urls) {
-  var i = 0;
-  var iFrameMarkup;
-  $('#iFrames').remove();
-  $('<div id="iFrames"></div>').appendTo('body');
-  for (i = 0; i < urls.length; i++) {
-    iFrameMarkup = iFrameMarkupTemplate;
-    iFrameMarkup = iFrameMarkup.replace('${url}', urls[i].url);
-    iFrameMarkup = iFrameMarkup.replace('${url}', urls[i].url);
-    iFrameMarkup = iFrameMarkup.replace('${idx}', urls[i].idx);
-    iFrameMarkup = iFrameMarkup.replace('${idx}', urls[i].idx);
-    iFrameMarkup = iFrameMarkup.replace('${x}', urls[i].x);
-    iFrameMarkup = iFrameMarkup.replace('${y}', urls[i].y);
-    iFrameMarkup = iFrameMarkup.replace('${width}', urls[i].width);
-    iFrameMarkup = iFrameMarkup.replace('${height}', urls[i].height);
-    $(iFrameMarkup).appendTo('#iFrames');
-  }
+    $('#iFrames')
+        .css('transition', 'transform 0')
+        .css('transform', 'translate(0px,0px) scale(1, 1)')
+        .empty();
+
+    urls.forEach((options) => {
+        const iFrameMarkup = getIframeMarkup(options);
+
+        $(iFrameMarkup).appendTo('#iFrames');
+    });
+}
+
+function displayStatus(status) {
+    const message = $('#statusMessage');
+
+    message
+        .text(status)
+        .animate({ opacity: 1 }, 1250, () => {
+            message.animate({ opacity: 0 }, 1250);
+        });
 }
 
 function getHeight(resolution, ratio) {
-  return (resolution / ratio.split('/')[0]) * ratio.split('/')[1];
+    const ratioSplit = ratio.split('/');
+
+    return (resolution / ratioSplit[0]) * ratioSplit[1];
 }
 
 function getPosition(idx, width, height) {
-  var position = {};
-  position.x = (idx % 3) * width;
-  position.y = Math.floor(idx / 3) * height;
-  return position;
-}
-
-function addCSSStyles() {
-  $('.iFrames').each(function () {
-    var x = $(this).data('x');
-    var y = $(this).data('y');
-    $(this).css('-webkit-transform', 'translate(' + x + 'px,' + y + 'px)');
-  });
-}
-
-function addCSSStylesWithScaleFix() {
-  $('.iFrames').each(function () {
-    var x = $(this).data('x');
-    var y = $(this).data('y');
-    $(this).css('-webkit-transform', 'translate(' + (x + 946) + 'px,' + (y + 330) + 'px) scale(1.74, 1.41)');
-  });
-}
-
-function getFramePosition(iframe) {
-  var nextiFrame = $('#iFrame_' + iframe).parent();
-  var x = nextiFrame.data('x');
-  var y = nextiFrame.data('y');
   return {
-    x: x,
-    y: y
+      x: (idx % 3) * width,
+      y: Math.floor(idx / 3) * height
   };
 }
 
-function nextPosition() {
-  if (options.random) {
-    nextIframe = Math.floor(Math.random() * options.nrOfiFrames);
-  } else {
-    nextIframe = nextIframe + 1;
-    if (nextIframe >= options.nrOfiFrames) {
-      nextIframe = 0;
-    }
-  }
-  return nextIframe;
+function getFramePosition(iframe) {
+    const nextiFrame = $('.iFrames').eq(iframe);
+
+    return nextiFrame.data();
 }
 
-function previousPosition() {
-  if (options.random) {
-    nextIframe = Math.floor(Math.random() * options.nrOfiFrames);
-  } else {
-    nextIframe = nextIframe - 1;
-    if (nextIframe < 0) {
-      nextIframe = options.nrOfiFrames - 1;
-    }
-  }
-  return nextIframe;
+function openOptions() {
+    chrome.app.window.create("../view/options.html", {
+      "bounds": {
+        "width": 684,
+        "height": 550
+      }
+    });
 }
 
+function addCSSStyles() {
+    $('.iFrames').each(function() {
+        const elem = $(this);
+        const { x, y}  = elem.data();
+
+        elem.css('-webkit-transform', `translate(${x}px, ${y}px)`);
+    });
+}
+
+function addCSSStylesWithScaleFix() {
+    $('.iFrames').each(function () {
+        const elem = $(this);
+        const { x, y } = elem.data();
+
+        elem.css('-webkit-transform', `translate(${x + 946}px, ${y + 330}px) scale(1.74, 1.41)`);
+    });
+}
+
+/*
 function translateToNextFrame() {
   var scalingX;
   switch (options.nrOfiFrames) {
@@ -125,159 +105,275 @@ function translateToNextFrame() {
     addCSSStylesWithScaleFix();
   }
 }
-
-function showOverview() {
-  overviewMode = true;
-  translateToNextFrame();
-}
-
-function animate(iframe) {
-  if (overviewMode) {
-    setTimeout(function () {
-      nextIframe = nextPosition();
-      if (options.reload) {
-        var url = $('#iFrame_' + nextIframe).data('url');
-        $('#iFrame_' + nextIframe).attr('src', url);
-      }
-    }, options.animation ? 5000 : 0);
-  } else {
-    translateToNextFrame();
-    setTimeout(function () {
-      var framePosition = getFramePosition(iframe);
-      $('#iFrames').css('-webkit-transform', 'translate(' + -framePosition.x + 'px,' + -framePosition.y + 'px) scale(1,1)');
-      if (options.chromeScalingFix) {
-        addCSSStyles();
-      }
-      nextIframe = nextPosition();
-      if (options.reload) {
-        var url = $('#iFrame_' + nextIframe).data('url');
-        $('#iFrame_' + nextIframe).attr('src', url);
-      }
-    }, options.animation ? 5000 : 0);
-  }
-}
+*/
 
 function loadOptions() {
-  chrome.storage.local.get(function (items) {
-    if (!chrome.runtime.lastError) {
-      if (items.hasOwnProperty('urls')) {
-        var urls = JSON.parse(items.urls);
-        options.reload = items.reload;
-        options.nrOfiFrames = urls.length;
-        options.duration = parseInt(items.duration, 10) * 1000;
-        options.resolution = items.resolution;
-        options.ratio = items.ratio;
-        options.random = items.random;
-        options.chromeScalingFix = items.chromeScalingFix;
-        var width = items.resolution;
-        var height;
-        var idx;
-        $('#iFrames').remove();
-        if (width !== 'full') {
-          height = getHeight(width, items.ratio);
-        } else {
-          width = $(document).width();
-          height = $(document).height();
-        }
-        var iFrameOptions = [];
-        var position;
-        for (idx = 0; idx < urls.length; idx++) {
-          position = getPosition(idx, width, height);
-          iFrameOptions[iFrameOptions.length] = {
-            url: urls[idx].url,
-            width: width,
-            height: height,
-            idx: idx,
-            x: position.x,
-            y: position.y
-          };
-        }
-        addIFrames(iFrameOptions);
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get((options) => {
+            if (chrome.runtime.lastError) {
+                reject(chrome.runtime.lastError);
+                return;
+            }
 
-        // TODO throw an event here!
-        if (!options.chromeScalingFix) {
-          addCSSStyles();
-        }
-        if (intervalTimer) {
-          clearInterval(intervalTimer);
-        }
-        intervalTimer = setInterval(function () {
-          animate(nextIframe);
-        }, options.duration);
-      } else {
-        // TODO throw an event here!
-        chrome.app.window.create("../view/options.html", {
-          "bounds": {
-            "width": 684,
-            "height": 550
-          }
+            if (options.hasOwnProperty('urls')) {
+                resolve(options);
+                return;
+            }
+
+            openOptions();
         });
-      }
-    } else {
-      console.error(chrome.runtime.lastError);
-    }
-  });
+    });
 }
 
-function nextFrame(frame) {
-  overviewMode = false;
-  if (frame < options.nrOfiFrames) {
-    animate(frame);
-  }
+class Urlando {
+
+    constructor() {
+        this.currentFrameNr = 0;
+        this.animationIsRunning = false;
+        this.isInOverview = false;
+        this.bindKeys();
+    }
+
+    init(options) {
+        this.options = this.prepareOptions(options);
+        this.render();
+    }
+
+    updateOptions() {
+        loadOptions()
+            .then(this.init.bind(this))
+            .catch(console.error); // eslint-disable-line
+    }
+
+    switchToRandomFrame() {
+        const { nrOfiFrames } = this.options;
+
+        this.currentFrameNr = Math.floor(Math.random() * nrOfiFrames);
+        this.transitionToFrame();
+    }
+
+    switchToNextFrame() {
+        const { nrOfiFrames } = this.options;
+        const nextFramNr = this.currentFrameNr + 1;
+
+        this.currentFrameNr = nextFramNr >= nrOfiFrames ? 0 : nextFramNr;
+        this.transitionToFrame();
+    }
+
+    switchToPrevFrame() {
+        const { nrOfiFrames } = this.options;
+        const prevFrameNr = this.currentFrameNr - 1;
+
+        this.currentFrameNr = prevFrameNr < 0 ? nrOfiFrames - 1 : prevFrameNr;
+        this.transitionToFrame();
+    }
+
+    prepareOptions(options){
+        const urls = JSON.parse(options.urls);
+        const {
+            reload,
+            duration,
+            resolution,
+            ratio,
+            random,
+            chromeScalingFix
+        } = options;
+
+        return Object.assign(
+            {},
+            this.options,
+            {
+                reload,
+                resolution,
+                ratio,
+                random,
+                chromeScalingFix,
+                urls,
+                nrOfiFrames: urls.length,
+                duration: parseInt(duration, 10) * 1000
+            }
+        );
+    }
+
+    render() {
+        const {
+            resolution,
+            ratio,
+            urls
+        } = this.options;
+        let width = resolution;
+        let height = 0;
+
+        if (resolution !== 'full') {
+            height = getHeight(resolution, ratio);
+        } else {
+            height = window.innerHeight;
+            width = window.innerWidth;
+        }
+
+        const iFrameOptions = urls.map(({ url }, idx) => {
+            const { x, y } = getPosition(idx, width, height);
+
+            return {
+                url,
+                width,
+                height,
+                idx,
+                x,
+                y
+            };
+        });
+
+        addIFrames(iFrameOptions);
+        this.applyStyles();
+    }
+
+    reloadFrame() {
+        const frame = $(`#iFrame_${this.currentFrameNr}`);
+        const { url } = frame.data();
+
+        frame.attr('src', url);
+    }
+
+    animate() {
+        const { random } = this.options;
+        const timeout = this.animationIsRunning ? 5000 : 0;
+
+        if (this.isInOverview) {
+            return;
+        }
+
+        this.showOverview();
+        setTimeout(() => {
+            random ? this.switchToRandomFrame() : this.switchToNextFrame();
+        }, timeout);
+    }
+
+    transitionToFrame() {
+        const { reload, chromeScalingFix } = this.options;
+        const { x, y } = getFramePosition(this.currentFrameNr);
+
+        if (reload) {
+            this.reloadFrame();
+        }
+
+        $('#iFrames').css('transform', `translate(${-x}px, ${-y}px) scale(1,1)`);
+
+        if (!chromeScalingFix) {
+            addCSSStyles();
+        } else {
+            addCSSStylesWithScaleFix();
+        }
+    }
+
+    applyStyles() {
+        const { chromeScalingFix } = this.options;
+
+        // TODO throw an event here!
+        if (!chromeScalingFix) {
+          addCSSStyles();
+        } else {
+            addCSSStylesWithScaleFix();
+        }
+    }
+
+    startAnimation() {
+        const { duration } = this.options;
+
+        if (this.animationIsRunning) {
+            return;
+        }
+
+        this.animationIsRunning = true;
+        displayStatus('animation started');
+        this.animationIntervalId = setInterval(this.animate.bind(this), duration);
+    }
+
+    stopAnimation() {
+        if (!this.animationIsRunning) {
+            return;
+        }
+
+        this.animationIsRunning = false;
+        displayStatus('animation stopped');
+        clearInterval(this.animationIntervalId);
+    }
+
+    toggleAnimation() {
+        this.animationIsRunning ? this.stopAnimation() : this.startAnimation();
+    }
+
+    showOverview() {
+        const { nrOfiFrames, chromeScalingFix } = this.options;
+        const scalingY = 1 / Math.ceil(nrOfiFrames / 3);
+        let scalingX;
+
+        switch (nrOfiFrames) {
+            case 1:
+                scalingX = 1;
+                break;
+            case 2:
+                scalingX = 1 / 2;
+                break;
+            default:
+                scalingX = 1 / 3;
+        }
+
+        $('#iFrames').css('-webkit-transform', `translate(0px,0px) scale(${scalingX}, ${scalingY})`);
+
+        if (chromeScalingFix) {
+            addCSSStylesWithScaleFix();
+        }
+    }
+
+    showFrame(event, keycombo) {
+        const { nrOfiFrames } = this.options;
+        const frameNr = parseInt(keycombo.replace('mod+', ''), 10) - 1;
+
+        if (frameNr >= nrOfiFrames) {
+            return;
+        }
+
+        this.currentFrameNr = frameNr;
+        this.transitionToFrame();
+    }
+
+    nextFrame() {
+        this.stopAnimation();
+        this.switchToNextFrame();
+    }
+
+    previousFrame() {
+        this.stopAnimation();
+        this.switchToPrevFrame();
+    }
+
+    bindKeys() {
+        bindKey({
+            'mod+s': this.toggleAnimation.bind(this),
+            'mod+o': openOptions,
+            'mod+0': this.showOverview.bind(this),
+            'mod+1': this.showFrame.bind(this),
+            'mod+2': this.showFrame.bind(this),
+            'mod+3': this.showFrame.bind(this),
+            'mod+4': this.showFrame.bind(this),
+            'mod+5': this.showFrame.bind(this),
+            'mod+6': this.showFrame.bind(this),
+            'mod+7': this.showFrame.bind(this),
+            'mod+8': this.showFrame.bind(this),
+            'mod+9': this.showFrame.bind(this),
+            'mod+right': this.nextFrame.bind(this),
+            'mod+left': this.previousFrame.bind(this)
+        });
+    }
+
 }
 
 $(function () {
-  chrome.runtime.onMessage.addListener(loadOptions);
-  Mousetrap.bind('mod+o', function () {
-    chrome.app.window.create("../view/options.html", {
-      "bounds": {
-        "width": 684,
-        "height": 550
-      }
-    });
-  });
-  Mousetrap.bind('mod+s', function () {
-    if (!options.animation) {
-      options.animation = true;
-      intervalTimer = setInterval(function () {
-        animate(nextIframe);
-      }, options.duration);
-      displayStatus('animation started');
-    } else {
-      options.animation = false;
-      if (intervalTimer) {
-        clearInterval(intervalTimer);
-      }
-      displayStatus('animation paused');
-    }
-  });
-  Mousetrap.bind('mod+0', showOverview);
-  Mousetrap.bind('mod+1', function () {
-    nextFrame(0);
-  });
-  Mousetrap.bind('mod+2', function () {
-    nextFrame(1);
-  });
-  Mousetrap.bind('mod+3', function () {
-    nextFrame(2);
-  });
-  Mousetrap.bind('mod+4', function () {
-    nextFrame(3);
-  });
-  Mousetrap.bind('mod+5', function () {
-    nextFrame(4);
-  });
-  Mousetrap.bind('mod+6', function () {
-    nextFrame(5);
-  });
-  Mousetrap.bind('mod+7', function () {
-    nextFrame(6);
-  });
-  Mousetrap.bind('mod+8', function () {
-    nextFrame(7);
-  });
-  Mousetrap.bind('mod+9', function () {
-    nextFrame(8);
-  });
-  loadOptions();
+    const urlando = new Urlando();
+
+    chrome.runtime.onMessage.addListener(urlando.updateOptions.bind(urlando));
+    loadOptions()
+        .then(urlando.init.bind(urlando))
+        .catch(console.error); // eslint-disable-line
 });
